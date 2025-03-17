34a0869f46a63e9c6e8ba5403a8926e44d31598d
gabriel <gett.912@gmail.com>
2025-03-17 15:47:11 -0600

    Funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-//asignacion del valor "amigos"
+//asignacion del valor "amigos".
 let amigos = [];
+//Funcion para agregar amigos.
+function agregarAmigo() {
+    //capturar el valor del campo de entrada.
+    const intputAmigo = document.getElementById('amigo');
+    const nombreAmigo = intputAmigo.value.trim();
+    //validar la entrada.
+    if(nombreAmigo == ""){
+        alert("inserte un nombre, por favor.");
+        return;
+    }
+    //validar que no haya nombres repetidos.
+    if(amigos.includes(nombreAmigo)) {
+        alert(`El nombre ${nombreAmigo} ya esta en la lista.`)
+        return;
+    }
+    //actualizar el array de amigos.
+    amigos.push(nombreAmigo);
+    //limpiar el campo de entrada.
+    intputAmigo.value = "";
+    actualizarLista(); //llama a la funcion "actualizarLista".
+}
